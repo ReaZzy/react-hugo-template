992c28ee9f66151cc802901d51b0ff7c459cd170
REAZZY <maxim.nebela@gmail.com>
2022-02-24 18:51:18 +0200

    refactor⚒

--- js/src/index.tsx
+++ js/src/index.tsx
@@ -1,57 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */
+
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { ReactPortal, useCallback, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-const elems: HTMLCollectionOf<Element> =
-  document.getElementsByTagName('component');
-const promises = [];
+const App: React.FC = () => {
+  const elems: HTMLCollectionOf<Element> =
+    document.getElementsByTagName('component');
+  const [portals, setPortals] = useState<ReactPortal[]>([]);
+  const importRender = useCallback(async (id: string): Promise<ReactPortal> => {
+    const module = await import(`./components/${id}`);
+    const component = React.createElement(module?.default);
+    const item = document.getElementById(id);
+    return ReactDOM.createPortal(component, item!);
+  }, []);
 
-const getElementAttrs = (el: Element) => {
-  const props: { [key: string]: string } = {};
-  [].slice.call(el.attributes).forEach(({ name, value }) => {
-    props[name] = value;
-    return true;
-  });
-  return props;
+  useEffect(() => {
+    const promises: Promise<ReactPortal>[] = [];
+
+    Array.prototype.slice.call(elems).forEach((item: Element) => {
+      if (item.hasAttribute('id') && !item.hasAttribute('isPage')) {
+        const id = item.attributes.getNamedItem('id')!.nodeValue!;
+        promises.push(importRender(id));
+      }
+    });
+
+    Promise.all(promises)
+      .then((values) => setPortals(values))
+      .catch((e) => console.error(e));
+  }, []);
+
+  return <BrowserRouter>{portals}</BrowserRouter>;
 };
 
-function withRoute(Component: React.FC) {
-  return function withRouteClosure() {
-    return (
-      <BrowserRouter>
-        <Component />
-      </BrowserRouter>
-    );
-  };
-}
-
-const importRender = (props: { [key: string]: string }): Promise<void> => {
-  const { id, ...rest } = props;
-  return new Promise((resolve, reject) => {
-    import(`./components/${id}`)
-      .then((module: React.FC) => {
-        const component = React.createElement(
-          /* eslint-disable-next-line */
-          withRoute((module as any).default),
-          rest,
-        );
-
-        const item = document.getElementById(id);
-        ReactDOM.render(component, item, () => {
-          resolve();
-        });
-      })
-      .catch(() => reject());
-  });
-};
-
-const checkReactComponents = () => {
-  /* eslint-disable-next-line */
-  for (const item of elems) {
-    if (item.hasAttribute('id')) {
-      promises.push(importRender(getElementAttrs(item)));
-    }
-  }
-};
-
-checkReactComponents();
+ReactDOM.render(<App />, document.getElementById('root'));
